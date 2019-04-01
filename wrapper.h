f672a383e99102972ab4ce2cfc37f2a1191d8afc
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2019-04-01 04:31:56 -0400

    more headers

--- wrapper.h
+++ wrapper.h
@@ -1,4 +1,13 @@
 #include <epan/epan.h>
 #include <epan/proto.h>
+#include <epan/print.h>
+#include <epan/timestamp.h>
+#include <epan/epan_dissect.h>
+#include <epan/ftypes/ftypes.h>
+#include <epan/reassemble.h>
+#include <epan/stream.h>
+#include <epan/follow.h>
+#include <epan/print_stream.h>
+
 #include <wsutil/plugins.h>
 #include <wsutil/privileges.h>
